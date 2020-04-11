082000c2dd65300b872a31ee46964097f29aa351
sotto97 <sotto.bw97@gmail.com>
2020-04-11 07:55:18 +0000

    [update] user/top user/show & edit view

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -2,10 +2,22 @@ class User::UsersController < ApplicationController
   def index
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.all
+  end
+
   def edit
+    @user = User.find(current_user.id)
   end
 
   def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
   end
 
   def hide
@@ -15,5 +27,11 @@ class User::UsersController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email ,:first_name, :last_name, :first_name_kana, :last_name_kana, :username, :profile_image_id, :address, :introduction, :age, :job)
   end
 end
