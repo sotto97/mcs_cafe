70e2dfd0c714f2c49ec2bbb3cdc99543748d977d
sotto97 <sotto.bw97@gmail.com>
2020-04-16 08:17:18 +0000

    [update]フォロー/フォロワーの一覧画面

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,7 @@
 class Relationship < ApplicationRecord
-	#自分をフォローしているユーザー
+	#自分をフォローしているユーザー followerクラスではなく、Userクラス
 	belongs_to :follower, class_name: "User"
-	#自分がフォローしているユーザー
+	#自分がフォローしているユーザー followingクラスではなく、Userクラス
 	belongs_to :following, class_name: "User"
 
 	validates :follower_id, presence: true
